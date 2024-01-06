676647cf5c3a0b528714466c770f60af08b99afc
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-01-05 22:47:33 -0800

    Initial stage of third level dropdown menu.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Watchlist elements
 const watchList = document.querySelector(".watchlist");
 const watchListParagraph = document.querySelector(".watchlist p");
 const watchListIcon = document.querySelector(".watchlist i");
@@ -7,11 +8,23 @@ const watchListDropdownParagraph = document.querySelector(
   ".watchlist-dropdown p"
 );
 
+// Third Level Elements
+const categoryOneUnorderedList = document.querySelector(".category-1 ul");
+const categoryOneUnorderedList_LiElements = categoryOneUnorderedList.children;
+
+for (let i = 0; i < categoryOneUnorderedList_LiElements.length; i++) {
+  categoryOneUnorderedList_LiElements[i].children[0].innerText;
+}
+
+// Third Level Dropdown Elements
+const thirdLevelDropdown = document.querySelector(".third-level-dropdown");
+
+
 document.addEventListener("click", (e) => {
   const isWatchList = e.target.matches(".watchlist");
   const isWatchListParagraph = e.target.matches(".watchlist p");
   const isWatchListIcon = e.target.matches(".watchlist i");
-  
+
   if (isWatchList || isWatchListParagraph || isWatchListIcon) {
     watchList.classList.toggle("active");
     watchListDropdown.classList.toggle("active");
@@ -19,6 +32,7 @@ document.addEventListener("click", (e) => {
 });
 
 document.addEventListener("mouseover", (e) => {
+
   if (
     e.target === watchListDropdown ||
     e.target === watchListDropdownParagraph ||
@@ -31,4 +45,70 @@ document.addEventListener("mouseover", (e) => {
     watchList.classList.remove("active");
     watchListDropdown.classList.remove("active");
   }
+
+  if (e.target.matches(".electronics a")) {
+    thirdLevelDropdown.style.opacity = "1";
+  }
+
+  if (e.target.matches(".motors a")) {
+    thirdLevelDropdown.style.opacity = "1";
+  }
+
+  if (e.target.matches(".fashion a")) {
+    thirdLevelDropdown.style.opacity = "1";
+  }
+
+  if (e.target.matches(".collectibles a")) {
+    thirdLevelDropdown.style.opacity = "1";
+  }
+
+  if (e.target.matches(".sports a")) {
+    thirdLevelDropdown.style.opacity = "1";
+  }
+
+  if (e.target.matches(".health a")) {
+    thirdLevelDropdown.style.opacity = "1";
+  }
+
+  if (e.target.matches(".industrial a")) {
+    thirdLevelDropdown.style.opacity = "1";
+  }
+
+  if (e.target.matches(".garden a")) {
+    thirdLevelDropdown.style.opacity = "1";
+  }
 });
+
+document.addEventListener("mouseout", (e) => {
+  if (e.target.matches(".electronics a")) {
+    thirdLevelDropdown.style.opacity = "0";
+  }
+
+  if (e.target.matches(".motors a")) {
+    thirdLevelDropdown.style.opacity = "0";
+  }
+
+  if (e.target.matches(".fashion a")) {
+    thirdLevelDropdown.style.opacity = "0";
+  }
+
+  if (e.target.matches(".collectibles a")) {
+    thirdLevelDropdown.style.opacity = "0";
+  }
+
+  if (e.target.matches(".sports a")) {
+    thirdLevelDropdown.style.opacity = "0";
+  }
+
+  if (e.target.matches(".health a")) {
+    thirdLevelDropdown.style.opacity = "0";
+  }
+
+  if (e.target.matches(".industrial a")) {
+    thirdLevelDropdown.style.opacity = "0";
+  }
+
+  if (e.target.matches(".garden a")) {
+    thirdLevelDropdown.style.opacity = "0";
+  }
+})
---
+++ text.js
@@ -0,0 +1,147 @@
+export const motorsCategoryOne = [
+"Auto and truck parts",
+  "Tools and supplies",
+  "Turbo and charges",
+  "Clothing and merchandise",
+  "Shock Absorbers",
+  "Electronic and GPS",
+  "Air intake",
+  "Vintage pieces"
+]
+
+export const motorsCategoryTwo = [
+"Motorcycle parts",
+  "Body and frame",
+  "Engine and parts",
+  "Accessories",
+  "Exhausts and systems",
+  "Rims",
+  "Deals",
+  "Sell on eBay"
+]
+
+export const fashionCategoryOne = [
+"Footwear",
+  "Women's clothing",
+  "Footwear for women",
+  "Mens clothing",
+  "Mens footwear",
+  "Watches",
+  "Jewelry"
+]
+
+export const fashionCategoryTwo = [
+  "Accessories for men",
+  "Accessories for women",
+  "Bags and wallets for women",
+  "Mens glasses",
+  "Women's glasses",
+  "Sneakers",
+  "Deals",
+  "Sell on eBay"
+]
+
+export const collectiblesCategoryOne = [
+  "Collectibles",
+  "Art",
+  "Action figures",
+  "Cartoon characters",
+  "Movie and TV characters",
+  "Diecast",
+  "Musical memorabilia",
+]
+
+export const collectiblesCategoryTwo = [
+  "Sports memorabilia",
+  "Trading card games",
+  "Antiques",
+  "Comics",
+  "Funko pops",
+  "Star wars",
+  "Deals",
+  "Sell on eBay"
+]
+
+export const sportsCategoryOne = [
+  "Cycling",
+  "Fitness, running and yoga",
+  "Fitness tech",
+  "Fishing",
+  "Camping",
+  "Scooters",
+  "Team sports"
+]
+
+export const sportsCategoryTwo = [
+  "Water sports",
+  "Winter sports",
+  "Box and MMA",
+  "Swimming",
+  "GPS and running waters",
+  "Garmin",
+  "Deals",
+  "Sell on eBay"
+]
+
+export const healthCategoryOne = [
+  "Beauty",
+  "Makeup",
+  "Health",
+  "K-beauty",
+  "Manicure and pedicure",
+  "Hair products",
+  "Skin products",
+  "Orthopedic products"
+]
+
+export const healthCategoryTwo = [
+  "Vitamins and food supplements",
+  "Shaving and waxing",
+  "Bath and personal hygiene",
+  "Oral hygiene",
+  "Massagers",
+  "Deals",
+  "Sell on eBay",
+]
+
+export const industrialCategoryOne = [
+  "Dental health care",
+  "Electronic equipments and supplies",
+  "Metallurgy and manufacturing",
+  "Motors and industrial automation",
+  "Heavy equipment parts",
+  "Light industrial tools",
+  "Inspection, measurement and testing equipments",
+]
+
+export const industrialCategoryTwo = [
+  "Parking and shipping",
+  "Office and supplies equipments",
+  "Printing and graphic arts",
+  "Restaurants and food service",
+  "Maintenance and safety",
+  "Deals",
+  "Sell on eBay",
+]
+
+export const homeCategoryOne = [
+  "Workshop tools and equipment",
+  "Patio, garden and outdoors",
+  "Home improvement",
+  "Kitchen, dinning and bar",
+  "Lamps, lights and fan",
+  "Interior decoration",
+  "Home organization",
+  "Home appliance"
+]
+
+export const homeCategoryTwo = [
+  "Toys",
+  "Pets",
+  "Crafts",
+  "Art supplies",
+  "Musical instruments",
+  "Jewelry and beads",
+  "Deals",
+  "Sell on eBay"
+]
